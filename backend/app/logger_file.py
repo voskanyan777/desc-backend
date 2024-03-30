8828fe105f9da15a3eeefaecf5d04a2b10dc7e74
voskanyan777 <159440969+voskanyan777@users.noreply.github.com>
2024-03-30 22:52:44 +0700

    added utf-8 try 2

--- backend/app/logger_file.py
+++ backend/app/logger_file.py
@@ -3,5 +3,6 @@ import logging
 
 BASE_DIR = Path(__file__).parent.parent
 logging.basicConfig(filename=BASE_DIR / 'logs.log', level=logging.INFO,
-                    format='%(asctime)s - %(levelname)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+                    format='%(asctime)s - %(levelname)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S', encoding='UTF-8')
+
 logger = logging.getLogger()
