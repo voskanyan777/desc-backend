0a1b3168262a1fcfdcc8a3751ba97082eb277f8b
voskanyan777 <159440969+voskanyan777@users.noreply.github.com>
2024-03-30 22:55:34 +0700

    fixed bug in db/config.py file

--- backend/db/config.py
+++ backend/db/config.py
@@ -3,19 +3,17 @@
 """
 
 import os
-from dotenv import load_dotenv
+from pathlib import Path
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
-load_dotenv()
+BASE_DIR = Path(__file__).parent
 
-
-# class Settings(BaseSettings):
-class Settings():
-    DB_HOST = os.getenv("DB_HOST")
-    DB_PORT = os.getenv("DB_PORT")
-    DB_USER = os.getenv("DB_USER")
-    DB_PASS = os.getenv("DB_PASS")
-    DB_NAME = os.getenv("DB_NAME")
+class Settings(BaseSettings):
+    DB_HOST: str
+    DB_PORT: str
+    DB_USER: str
+    DB_PASS: str
+    DB_NAME: str
 
     @property
     def DATABASE_URL_psycopg(self):
@@ -23,8 +21,7 @@ class Settings():
         # postgresql+psycopg://postgres:postgres@localhost:5432/sa
         return f"postgresql+psycopg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
 
-    # model_config = SettingsConfigDict(env_file=f'{os.getcwd()}\\.env')
-    # model_config = SettingsConfigDict(env_file='.env')
+    model_config = SettingsConfigDict(env_file=BASE_DIR / '.env')
 
 
 settings = Settings()
